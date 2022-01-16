abe8cadd5d237ed38e5772219a235546bcca4123
gushanma <1421655736@qq.com>
2022-01-16 14:58:27 +0800

    原始状态

--- nx-stock/src/main/java/com/nx/stock/StockApplication.java
+++ nx-stock/src/main/java/com/nx/stock/StockApplication.java
@@ -2,9 +2,8 @@ package com.nx.stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+
 @SpringBootApplication
 public class StockApplication {
     public static void main(String[] args) {
