247638c8fd429b7ab22cf85cdc992c38466201af
shanyuan <1421655736@qq.com>
2022-08-17 14:36:51 +0800

    nacos 作为注册中心 讲课

--- msb-order/src/main/java/com/msb/order/OrderApplication.java
+++ msb-order/src/main/java/com/msb/order/OrderApplication.java
@@ -1,17 +1,30 @@
 package com.msb.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OrderApplication {
 
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
+        /*RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setInterceptors(Collections.singletonList(new LoadBalancerInterceptor(loadBalancerClient)));
+        return restTemplate;*/
     }
 
     public static void main(String[] args) {
--- msb-order/src/main/java/com/msb/order/controller/OrderController.java
+++ msb-order/src/main/java/com/msb/order/controller/OrderController.java
@@ -1,10 +1,17 @@
 package com.msb.order.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+@Slf4j
 @RestController
 public class OrderController {
     @Autowired
@@ -12,10 +19,16 @@ public class OrderController {
 
     @GetMapping("/order/create")
     public String createOrder(Integer productId,Integer userId){
-        String product = restTemplate.getForObject("http://localhost:9001/product/" + productId, String.class);
-        String userName = restTemplate.getForObject("http://localhost:10001/user/" + userId, String.class);
-        String result = restTemplate.getForObject("http://localhost:11001/stock/reduce//" + productId, String.class);
-        String shopCartResult = restTemplate.getForObject("http://localhost:12001/shopcart/remove?productId=" + productId +"&userId=" + userId, String.class);
-        return "用户：" + userName + "  购买了商品:" + product + " " + result + "  " + shopCartResult;
+//        String product = restTemplate.getForObject("http://localhost:9001/product/" + productId, String.class);
+//        String userName = restTemplate.getForObject("http://localhost:10001/user/" + userId, String.class);
+//        String result = restTemplate.getForObject("http://localhost:11001/stock/reduce//" + productId, String.class);
+//        String shopCartResult = restTemplate.getForObject("http://localhost:12001/shopcart/remove?productId=" + productId +"&userId=" + userId, String.class);
+//        return "用户：" + userName + "  购买了商品:" + product + " " + result + "  " + shopCartResult;
+        //此时RestTemplate 并不能识别我们的msb-stock
+        // 1、获取msb-stock列表
+        // 2、选择一个进行调用
+        //
+        String result = restTemplate.getForObject("http://msb-stock/stock/reduce/" + productId,String.class);
+        return "下单成功";
     }
 }
